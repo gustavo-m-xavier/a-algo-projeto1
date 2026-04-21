bc779b3c930b76e1f83f9e61e82300ab5495c925
trsaints <thiago.dev2k3@gmail.com>
2026-04-21 19:03:12 -0300

    update(backend): force monitoring overlay execution to be injected on browser page

--- backend/src/services/element-selection.ts
+++ backend/src/services/element-selection.ts
@@ -1,5 +1,6 @@
 import { Page } from "puppeteer";
 import { SelectedElement } from "../types/SelectedElement";
+import path from "path";
 
 /**
  * Seleciona um elemento na página e retorna suas informações.
@@ -8,105 +9,17 @@ import { SelectedElement } from "../types/SelectedElement";
  */
 export async function selectElementAsync(page: Page): Promise<SelectedElement> {
 	return new Promise(async (resolve) => {
+		// expõe callback para o browser
 		await page.exposeFunction("elementSelected", (data: SelectedElement) => {
 			resolve(data);
 		});
 
-		await page.evaluate(() => {
-			const createdButton = createOverlay();
+		// injeta o script no MAIN WORLD
+		const overlayPath = path.resolve(__dirname, "../browser/overlay.js");
 
-			console.log("Overlay injetado na página.")
+		await page.addScriptTag({ path: overlayPath });
 
-			createdButton.addEventListener(
-				"click",
-				(event) => handleElementSelection(event, createdButton),
-				{ capture: true },
-			);
-
-			/**
-			 * Renderiza um controle de sobreposição para realizar o monitoramento de elementos da página.
-			 * @returns O controle responsável por iniciar o monitoramento de cliques na página.
-			 */
-			function createOverlay(): HTMLButtonElement {
-				const createdButton = document.createElement("button");
-
-				createdButton.innerText = "Monitorar o click";
-				createdButton.style.position = "fixed";
-				createdButton.style.top = "10px";
-				createdButton.style.right = "10px";
-				createdButton.style.zIndex = "9999";
-				createdButton.style.backgroundColor = "rgba(0, 63, 117, 0.9)";
-				createdButton.style.color = "white";
-				createdButton.style.padding = "1rem";
-				createdButton.style.boxShadow = "rgba(0,0,0,0.24) 0px 3px 8px";
-
-				document.body.appendChild(createdButton);
-
-				return createdButton;
-			}
-
-			/**
-			 * Manipula o evento de clique no controle de sobreposição, iniciando o monitoramento do próximo clique na página para capturar o elemento selecionado.
-			 * @param event O evento de clique que aciona o callback.
-			 * @param overlay O controle de sobreposição que inicia o monitoramento de cliques na página.
-			 * @remarks O monitoramento é configurado para capturar apenas o próximo clique na página, ignorando cliques subsequentes até que o processo seja reiniciado.
-			 */
-			function handleElementSelection(event: PointerEvent, overlay: HTMLButtonElement) {
-				event.preventDefault();
-				event.stopPropagation();
-
-				overlay.innerText = "Monitorando click...";
-
-				document.addEventListener(
-					"mousedown",
-					(event) => {
-						event.preventDefault();
-						event.stopPropagation();
-
-						const el = event.target as HTMLElement;
-
-						if (el === overlay) {
-							return;
-						}
-
-						const data = {
-							selector: getFullSelector(el),
-							text: el.innerText || "",
-						};
-
-						(window as any).elementSelected(data);
-					},
-					{ once: true, capture: true },
-				);
-			}
-
-			/**
-			 * Obtém o seletor completo de um elemento, incluindo índices para elementos irmãos do mesmo tipo.
-			 * @param el o elemento HTML para o qual o seletor deve ser gerado.
-			 * @returns uma string representando o caminho completo do seletor CSS para o elemento fornecido.
-			 */
-			function getFullSelector(el: HTMLElement): string {
-				let path = "";
-
-				while (el.parentElement) {
-					let tag = el.tagName.toLowerCase();
-
-					const siblings = Array.from(el.parentElement.children).filter(
-						(child) => child.tagName === el.tagName,
-					);
-
-					if (siblings.length > 1) {
-						const index = siblings.indexOf(el) + 1;
-						tag += `:nth-of-type(${index})`;
-					}
-
-					path = tag + (path ? " > " + path : "");
-					el = el.parentElement;
-				}
-
-				return path;
-			}
-		});
+		// espera o script carregar
+		await page.waitForFunction(() => (window as any).__overlayLoaded === true);
 	});
 }
-
