33b3d71b176d70c3678dcd1848d9b6eceab80017
trsaints <thiago.dev2k3@gmail.com>
2026-04-21 14:01:15 -0300

    docs(backend): include docstring for core utility for retrieving element data

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,7 @@
 import puppeteer from "puppeteer";
 import express from "express";
 import cors from "cors";
-import { selectElement } from "./services/selectElement";
+import { selectElementAsync } from "./services/selectElement";
 import { monitorElement } from "./services/monitor";
 import { isValidUrl } from "./utils/urlValidator";
 
@@ -11,50 +11,50 @@ app.use(express.json());
 app.use(cors());
 
 app.post("/monitor", async (req, res) => {
-  const { url } = req.body;
+	const { url } = req.body;
 
-  if (!url) {
-    return res.status(400).json({ error: "A url é obrigatória" });
-  }
+	if (!url) {
+		return res.status(400).json({ error: "A url é obrigatória" });
+	}
 
-  if (!isValidUrl(url)) {
-    return res.status(400).json({ error: "A url é inválida" });
-  }
+	if (!isValidUrl(url)) {
+		return res.status(400).json({ error: "A url é inválida" });
+	}
 
-  const browser = await puppeteer.launch({
-    headless: false,
-    defaultViewport: null,
-  });
+	const browser = await puppeteer.launch({
+		headless: false,
+		defaultViewport: null,
+	});
 
-  const page = await browser.newPage();
+	const page = await browser.newPage();
 
-  await page.goto(url);
+	await page.goto(url);
 
-  console.log("Página carregada!");
-  console.log("Esperando o usuário clicar em um elemento...");
+	console.log("Página carregada!");
+	console.log("Esperando o usuário clicar em um elemento...");
 
-  const selected = await selectElement(page);
+	const selected = await selectElementAsync(page);
 
-  console.log("Elemento Selecionado...", selected);
+	console.log("Elemento Selecionado...", selected);
 
-  const newValue = await monitorElement(
-    browser,
-    page,
-    selected.selector,
-    selected.text,
-  );
+	const newValue = await monitorElement(
+		browser,
+		page,
+		selected.selector,
+		selected.text,
+	);
 
-  res.json({
-    message: "Mudança capturada com sucesso!",
-    data: {
-      oldValue: selected.text,
-      newValue: newValue,
-    },
-  });
+	res.json({
+		message: "Mudança capturada com sucesso!",
+		data: {
+			oldValue: selected.text,
+			newValue: newValue,
+		},
+	});
 
-  browser.close();
+	browser.close();
 });
 
 app.listen(3000, () => {
-  console.log("Servidor rodando em: http://localhost:3000");
+	console.log("Servidor rodando em: http://localhost:3000");
 });
--- backend/src/services/selectElement.ts
+++ backend/src/services/selectElement.ts
@@ -1,81 +1,91 @@
 import { Page } from "puppeteer";
 import { SelectedElement } from "../types/SelectedElement";
 
-export async function selectElement(page: Page): Promise<SelectedElement> {
-  return new Promise(async (resolve) => {
-    await page.exposeFunction("elementSelected", (data: SelectedElement) => {
-      resolve(data);
-    });
+/**
+ * Seleciona um elemento na página e retorna suas informações.
+ * @param page a instância da página do Puppeteer onde o elemento será selecionado.
+ * @returns uma Promise que resolve com um objeto contendo o seletor CSS completo e o texto do elemento selecionado.
+ */
+export async function selectElementAsync(page: Page): Promise<SelectedElement> {
+	return new Promise(async (resolve) => {
+		await page.exposeFunction("elementSelected", (data: SelectedElement) => {
+			resolve(data);
+		});
 
-    await page.evaluate(() => {
-      function getFullSelector(el: HTMLElement): string {
-        let path = "";
+		await page.evaluate(() => {
+			const createdButton = document.createElement("button");
 
-        while (el.parentElement) {
-          let tag = el.tagName.toLowerCase();
+			createdButton.innerText = "Monitorar o click";
+			createdButton.style.position = "fixed";
+			createdButton.style.top = "10px";
+			createdButton.style.right = "10px";
+			createdButton.style.zIndex = "9999";
+			createdButton.style.backgroundColor = "rgba(0, 63, 117, 0.9)";
+			createdButton.style.color = "white";
+			createdButton.style.padding = "1rem";
+			createdButton.style.boxShadow = "rgba(0,0,0,0.24) 0px 3px 8px";
 
-          const siblings = Array.from(el.parentElement.children).filter(
-            (child) => child.tagName === el.tagName,
-          );
+			document.body.appendChild(createdButton);
 
-          if (siblings.length > 1) {
-            const index = siblings.indexOf(el) + 1;
-            tag += `:nth-of-type(${index})`;
-          }
+			createdButton.addEventListener(
+				"click",
+				(event) => {
+					event.preventDefault();
+					event.stopPropagation();
 
-          path = tag + (path ? " > " + path : "");
-          el = el.parentElement;
-        }
+					createdButton.innerText = "Monitorando click...";
 
-        return path;
-      }
+					document.addEventListener(
+						"mousedown",
+						(event) => {
+							event.preventDefault();
+							event.stopPropagation();
 
-      const createdButton = document.createElement("button");
+							const el = event.target as HTMLElement;
 
-      createdButton.innerText = "Monitorar o click";
-      createdButton.style.position = "fixed";
-      createdButton.style.top = "10px";
-      createdButton.style.right = "10px";
-      createdButton.style.zIndex = "9999";
-      createdButton.style.backgroundColor = "rgba(0, 63, 117, 0.9)";
-      createdButton.style.color = "white";
-      createdButton.style.padding = "1rem";
-      createdButton.style.boxShadow = "rgba(0,0,0,0.24) 0px 3px 8px";
+							if (el === createdButton) {
+								return;
+							}
 
-      document.body.appendChild(createdButton);
+							const data = {
+								selector: getFullSelector(el),
+								text: el.innerText || "",
+							};
 
-      createdButton.addEventListener(
-        "click",
-        (event) => {
-          event.preventDefault();
-          event.stopPropagation();
+							(window as any).elementSelected(data);
+						},
+						{ once: true, capture: true },
+					);
+				},
+				{ capture: true },
+			);
+		});
+	});
+}
 
-          createdButton.innerText = "Monitorando click...";
+/**
+ * Obtém o seletor completo de um elemento, incluindo índices para elementos irmãos do mesmo tipo.
+ * @param el o elemento HTML para o qual o seletor deve ser gerado.
+ * @returns uma string representando o caminho completo do seletor CSS para o elemento fornecido.
+ */
+function getFullSelector(el: HTMLElement): string {
+	let path = "";
 
-          document.addEventListener(
-            "mousedown",
-            (event) => {
-              event.preventDefault();
-              event.stopPropagation();
+	while (el.parentElement) {
+		let tag = el.tagName.toLowerCase();
 
-              const el = event.target as HTMLElement;
+		const siblings = Array.from(el.parentElement.children).filter(
+			(child) => child.tagName === el.tagName,
+		);
 
-              if (el === createdButton) {
-                return;
-              }
+		if (siblings.length > 1) {
+			const index = siblings.indexOf(el) + 1;
+			tag += `:nth-of-type(${index})`;
+		}
 
-              const data = {
-                selector: getFullSelector(el),
-                text: el.innerText || "",
-              };
+		path = tag + (path ? " > " + path : "");
+		el = el.parentElement;
+	}
 
-              (window as any).elementSelected(data);
-            },
-            { once: true, capture: true },
-          );
-        },
-        { capture: true },
-      );
-    });
-  });
+	return path;
 }
