938a47c153bf68acfd99209340bd4eabdfa43bd2
trsaints <thiago.dev2k3@gmail.com>
2026-04-21 17:26:41 -0300

    update(backend): ensure expected paths match current declarations on 'api/schema.ts'

--- backend/src/api/paths.ts
+++ backend/src/api/paths.ts
@@ -1,14 +1,17 @@
 import { errorResponseRefs, jsonBody, jsonResponse, Paths, schemaRef } from "@apexjs-org/openapi";
 
+/**
+ * Define a documentação de endpoints para a rota de monitoramento.
+ */
 export const monitorPaths: Paths = {
 	'/monitor': {
 		post: {
 			operationId: 'monitorPage',
 			summary: 'Monitora uma página web para mudanças em um elemento selecionado',
-			requestBody: jsonBody(schemaRef('monitorRequestBody')),
+			requestBody: jsonBody(schemaRef('monitorRequest')),
 			responses: {
 				...errorResponseRefs(),
-				'200': jsonResponse(schemaRef('monitorResponseBody')),
+				'200': jsonResponse(schemaRef('monitorResponse')),
 			}
 		}
 	}
