149b3d92de280ad2c73d54664bae5ce098a56848
trsaints <thiago.dev2k3@gmail.com>
2026-04-21 18:23:58 -0300

    fix(backend): move helper functions inside page evaluation for enabling browser availability

--- backend/src/services/selectElement.ts
+++ backend/src/services/selectElement.ts
@@ -20,6 +20,63 @@ export async function selectElementAsync(page: Page): Promise<SelectedElement> {
 				(event) => handleElementSelection(event, createdButton),
 				{ capture: true },
 			);
+
+			/**
+			 * Renderiza um controle de sobreposição para realizar o monitoramento de elementos da página.
+			 * @returns O controle responsável por iniciar o monitoramento de cliques na página.
+			 */
+			function createOverlay(): HTMLButtonElement {
+				const createdButton = document.createElement("button");
+
+				createdButton.innerText = "Monitorar o click";
+				createdButton.style.position = "fixed";
+				createdButton.style.top = "10px";
+				createdButton.style.right = "10px";
+				createdButton.style.zIndex = "9999";
+				createdButton.style.backgroundColor = "rgba(0, 63, 117, 0.9)";
+				createdButton.style.color = "white";
+				createdButton.style.padding = "1rem";
+				createdButton.style.boxShadow = "rgba(0,0,0,0.24) 0px 3px 8px";
+
+				document.body.appendChild(createdButton);
+
+				return createdButton;
+			}
+
+			/**
+			 * Manipula o evento de clique no controle de sobreposição, iniciando o monitoramento do próximo clique na página para capturar o elemento selecionado.
+			 * @param event O evento de clique que aciona o callback.
+			 * @param overlay O controle de sobreposição que inicia o monitoramento de cliques na página.
+			 * @remarks O monitoramento é configurado para capturar apenas o próximo clique na página, ignorando cliques subsequentes até que o processo seja reiniciado.
+			 */
+			function handleElementSelection(event: PointerEvent, overlay: HTMLButtonElement) {
+				event.preventDefault();
+				event.stopPropagation();
+
+				overlay.innerText = "Monitorando click...";
+
+				document.addEventListener(
+					"mousedown",
+					(event) => {
+						event.preventDefault();
+						event.stopPropagation();
+
+						const el = event.target as HTMLElement;
+
+						if (el === overlay) {
+							return;
+						}
+
+						const data = {
+							selector: getFullSelector(el),
+							text: el.innerText || "",
+						};
+
+						(window as any).elementSelected(data);
+					},
+					{ once: true, capture: true },
+				);
+			}
 		});
 	});
 }
@@ -51,59 +108,3 @@ function getFullSelector(el: HTMLElement): string {
 	return path;
 }
 
-/**
- * Renderiza um controle de sobreposição para realizar o monitoramento de elementos da página.
- * @returns O controle responsável por iniciar o monitoramento de cliques na página.
- */
-function createOverlay(): HTMLButtonElement {
-	const createdButton = document.createElement("button");
-
-	createdButton.innerText = "Monitorar o click";
-	createdButton.style.position = "fixed";
-	createdButton.style.top = "10px";
-	createdButton.style.right = "10px";
-	createdButton.style.zIndex = "9999";
-	createdButton.style.backgroundColor = "rgba(0, 63, 117, 0.9)";
-	createdButton.style.color = "white";
-	createdButton.style.padding = "1rem";
-	createdButton.style.boxShadow = "rgba(0,0,0,0.24) 0px 3px 8px";
-
-	document.body.appendChild(createdButton);
-
-	return createdButton;
-}
-
-/**
- * Manipula o evento de clique no controle de sobreposição, iniciando o monitoramento do próximo clique na página para capturar o elemento selecionado.
- * @param event O evento de clique que aciona o callback.
- * @param overlay O controle de sobreposição que inicia o monitoramento de cliques na página.
- * @remarks O monitoramento é configurado para capturar apenas o próximo clique na página, ignorando cliques subsequentes até que o processo seja reiniciado.
- */
-function handleElementSelection(event: PointerEvent, overlay: HTMLButtonElement) {
-	event.preventDefault();
-	event.stopPropagation();
-
-	overlay.innerText = "Monitorando click...";
-
-	document.addEventListener(
-		"mousedown",
-		(event) => {
-			event.preventDefault();
-			event.stopPropagation();
-
-			const el = event.target as HTMLElement;
-
-			if (el === overlay) {
-				return;
-			}
-
-			const data = {
-				selector: getFullSelector(el),
-				text: el.innerText || "",
-			};
-
-			(window as any).elementSelected(data);
-		},
-		{ once: true, capture: true },
-	);
-}
